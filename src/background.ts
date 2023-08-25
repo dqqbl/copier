bedb4f9c8ce7c2ef0b877c341ccf819bbd466cfa
dqqbl <deemoto2020@gmail.com>
2023-08-25 16:06:08 +0800

    禁用copy冒泡-fix csdn无法复制 / 掘金添加尾缀

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,4 @@
-import { getCurrentTab, getDomain } from "./utils";
+import { getCurrentTab, getDomain } from './utils';
 
 /** 安装初始化 */
 chrome.runtime.onInstalled.addListener(({ reason }) => {
@@ -15,19 +15,23 @@ chrome.runtime.onInstalled.addListener(({ reason }) => {
 /** 监听域名变动 */
 chrome.webNavigation.onCommitted.addListener((details) => {
   const { url, tabId, frameId } = details;
-  if (details?.url.startsWith("http")) {
-    chrome.storage.sync.get(["whiteList"], (result) => {
+  if (details?.url.startsWith('http')) {
+    chrome.storage.sync.get(['whiteList'], (result) => {
       if (result.whiteList.includes(getDomain(url))) {
         // @ts-ignore
         chrome.scripting.removeCSS({
           target: { tabId },
-          files: ["inject.css"],
+          files: ['inject.css'],
         });
-      // 仅主页面注入一次，避免重复注入
+        // 仅主页面注入一次，避免重复注入
       } else if (frameId === 0) {
         chrome.scripting.insertCSS({
           target: { tabId },
-          files: ["inject.css"],
+          files: ['inject.css'],
+        });
+        chrome.scripting.executeScript({
+          target: { tabId },
+          func: disableCopyPopup,
         });
       }
     });
@@ -36,18 +40,37 @@ chrome.webNavigation.onCommitted.addListener((details) => {
 
 chrome.storage.onChanged.addListener(async (changes) => {
   const tab = await getCurrentTab();
-  if (changes.whiteList?.newValue && tab?.url.startsWith("http")) {
+  if (changes.whiteList?.newValue && tab?.url.startsWith('http')) {
     if (changes.whiteList?.newValue.includes(getDomain(tab?.url))) {
       // @ts-ignore
       chrome.scripting.removeCSS({
         target: { tabId: tab.id },
-        files: ["inject.css"],
+        files: ['inject.css'],
       });
     } else {
       chrome.scripting.insertCSS({
         target: { tabId: tab.id },
-        files: ["inject.css"],
+        files: ['inject.css'],
+      });
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: disableCopyPopup,
       });
     }
   }
 });
+
+const disableCopyPopup = () => {
+  window.addEventListener('copy', (e) => {
+    //取消默认复制
+    // e.preventDefault();
+    e.stopPropagation();
+    // let content = window.getSelection();
+    //获取复制的text
+    // let textContent = content.toString();
+    //获取复制的html
+    // let htmlContent = content.getRangeAt(0).cloneContents();
+    // //@ts-ignore 给剪切板赋值
+    // e.clipboardData.setData('text/plain', textContent);
+  }, true);
+};
